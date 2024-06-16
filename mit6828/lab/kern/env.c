c174c7192af9110b55b17ca04bd6cf05d3ba2bd7
crazyofcode <1710338943@qq.com>
2024-06-16 17:11:34 +0800

    lab5

--- mit6828/lab/kern/env.c
+++ mit6828/lab/kern/env.c
@@ -273,7 +273,7 @@ env_alloc(struct Env **newenv_store, envid_t parent_id)
 	env_free_list = e->env_link;
 	*newenv_store = e;
 
-	cprintf("[%08x] new env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
+	// cprintf("[%08x] new env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
 	return 0;
 }
 
@@ -415,6 +415,9 @@ env_create(uint8_t *binary, enum EnvType type)
 	// The new env's parent ID is set to 0.
 	e->env_parent_id = 0;
 	load_icode(e, binary);
+
+	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
+	// LAB 5: Your code here.
 }
 
 //
@@ -434,7 +437,7 @@ env_free(struct Env *e)
 		lcr3(PADDR(kern_pgdir));
 
 	// Note the environment's demise.
-	cprintf("[%08x] free env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
+	// cprintf("[%08x] free env %08x\n", curenv ? curenv->env_id : 0, e->env_id);
 
 	// Flush all mapped pages in the user portion of the address space
 	static_assert(UTOP % PTSIZE == 0);
--- mit6828/lab/kern/init.c
+++ mit6828/lab/kern/init.c
@@ -48,6 +48,9 @@ i386_init(void)
 	// Starting non-boot CPUs
 	boot_aps();
 
+	// Start fs.
+	ENV_CREATE(fs_fs, ENV_TYPE_FS);
+
 #if defined(TEST)
 	// Don't touch -- used by grading script!
 	ENV_CREATE(TEST, ENV_TYPE_USER);
@@ -56,9 +59,12 @@ i386_init(void)
 	// ENV_CREATE(user_yield, ENV_TYPE_USER);
 	// ENV_CREATE(user_yield, ENV_TYPE_USER);
 	// ENV_CREATE(user_yield, ENV_TYPE_USER);
-	// ENV_CREATE(user_primes, ENV_TYPE_USER);
+	// ENV_CREATE(user_icode, ENV_TYPE_USER);
   ENV_CREATE(user_spin, ENV_TYPE_USER);
 #endif // TEST*
+
+	// Should not be necessary - drains keyboard because interrupt has given up.
+	kbd_intr();
 
 	// Schedule and run the first user environment!
 	sched_yield();
