2bcfb5cc8c985e46a32b5d6fa64e3993fc6119e3
crazyofcode <1710338943@qq.com>
2024-08-11 23:08:07 +0800

    cs61a lab09

--- lab09/lab09/lab09.scm
+++ lab09/lab09/lab09.scm
@@ -1,10 +1,26 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (if (< num1 num2)
+    -1
+      (if (= num1 num2)
+        0
+          1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (inc)
+    (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x)
+    (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (define (compose-n f n)
+    (if (= n 1)
+        f
+        (composed f (compose-n f (- n 1)))))
+  (lambda (x)
+    ((compose-n f n) x)))
 
 (define (max a b)
   (if (> a b)
@@ -16,12 +32,26 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define gcd
+  (lambda (a b)
+    (if (= b 0)
+        a
+        (gcd b (remainder a b)))))
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst)
+  (if (null? lst)
+      '()
+      (cons (car lst) 
+            (cons (car lst) 
+                  (duplicate (cdr lst))))))
 
 (expect (duplicate '(1 2 3)) (1 1 2 2 3 3))
 
 (expect (duplicate '(1 1)) (1 1 1 1))
 
-(define (deep-map fn s) 'YOUR-CODE-HERE)
+(define (deep-map fn s)
+  (map (lambda (x)
+         (if (list? x)
+             (deep-map fn x)
+             (fn x)))
+       s))
